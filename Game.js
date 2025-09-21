588cf31aa246d086a42a9e5d20410022ec0adc1c
oktiw6666-ops <oktiw6666@gmail.com>
2025-09-21 12:05:03 +0700

    Create Game.js

---
+++ Game.js
@@ -0,0 +1,84 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let player;
+let cursors;
+let platforms;
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image("sky", "https://labs.phaser.io/assets/skies/space3.png");
+    this.load.image("ground", "https://labs.phaser.io/assets/platform.png");
+    this.load.spritesheet("dude",
+        "https://labs.phaser.io/assets/sprites/dude.png",
+        { frameWidth: 32, frameHeight: 48 }
+    );
+}
+
+function create() {
+    this.add.image(400, 300, "sky").setDisplaySize(config.width, config.height);
+
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 584, "ground").setScale(2).refreshBody();
+
+    player = this.physics.add.sprite(100, 450, "dude");
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: "turn",
+        frames: [ { key: "dude", frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player, platforms);
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play("left", true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play("right", true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play("turn");
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
+}
